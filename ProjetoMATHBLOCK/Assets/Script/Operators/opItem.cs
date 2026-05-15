a494da255d04fed37fb849dcd86834a1d1fc1a4d
Yanszpereira <143100710+Yanszpereira@users.noreply.github.com>
2026-05-15 18:41:01 -0300

    Add - Modelos: Pedestal, operadores e cubos refinados

--- ProjetoMATHBLOCK/Assets/Script/Operators/opItem.cs
+++ ProjetoMATHBLOCK/Assets/Script/Operators/opItem.cs
@@ -4,13 +4,29 @@ public class opItem : MonoBehaviour
 {
     public GravityInteract.PencilOperator operatorType;
 
+    [Header("Visual Wobble")]
+    [SerializeField] private bool useWobble = true;
+    [SerializeField] private float wobbleAmplitude = 0.15f;
+    [SerializeField] private float wobbleSpeed = 1.5f;
+
     private Vector3 originalPosition;
     private Quaternion originalRotation;
+    private float wobbleTimeOffset;
 
     private void Awake()
     {
         originalPosition = transform.position;
         originalRotation = transform.rotation;
+        wobbleTimeOffset = Random.Range(0f, Mathf.PI * 2f);
+    }
+
+    private void Update()
+    {
+        if (!useWobble)
+            return;
+
+        float verticalOffset = Mathf.Sin((Time.time * wobbleSpeed) + wobbleTimeOffset) * wobbleAmplitude;
+        transform.position = originalPosition + Vector3.up * verticalOffset;
     }
 
     public void ConsumeFromScene()
