1d6f43ff90936d7fb494bf696a21bfd6c1456ab9
Yanszpereira <143100710+Yanszpereira@users.noreply.github.com>
2026-06-09 22:11:12 -0300

    Melhora variedade das solucoes procedurais do MathBlock

--- ProjetoMATHBLOCK/Assets/Script/Pads/DoorValueVerifier.cs
+++ ProjetoMATHBLOCK/Assets/Script/Pads/DoorValueVerifier.cs
@@ -3,10 +3,41 @@ using UnityEngine;
 public class DoorValueVerifier : MonoBehaviour
 {
     [SerializeField] private int requiredValue = 1;
+    [SerializeField, HideInInspector] private bool useRequiredRange;
+    [SerializeField, HideInInspector] private int requiredMinValue = 1;
+    [SerializeField, HideInInspector] private int requiredMaxValue = 1;
     [SerializeField] private GameObject acceptedPadObject;
     [SerializeField] private GameObject successParticleObject;
 
     public int RequiredValue => requiredValue;
+    public int RequiredMinValue => useRequiredRange ? requiredMinValue : requiredValue;
+    public int RequiredMaxValue => useRequiredRange ? requiredMaxValue : requiredValue;
+
+    private void OnValidate()
+    {
+        requiredValue = Mathf.Max(0, requiredValue);
+        requiredMinValue = Mathf.Max(0, requiredMinValue);
+        requiredMaxValue = Mathf.Max(requiredMinValue, requiredMaxValue);
+    }
+
+    public void SetRequiredRange(int minValue, int maxValue)
+    {
+        requiredMinValue = Mathf.Max(0, Mathf.Min(minValue, maxValue));
+        requiredMaxValue = Mathf.Max(requiredMinValue, Mathf.Max(minValue, maxValue));
+        requiredValue = requiredMinValue;
+        useRequiredRange = true;
+
+        if (requiredMinValue == requiredMaxValue)
+        {
+            Debug.Log($"Verificador {name}: valor exigido configurado para {requiredMinValue}.");
+            Debug.Log($"[TEMP] Valor da porta: {requiredMinValue}.");
+        }
+        else
+        {
+            Debug.Log($"Verificador {name}: intervalo exigido configurado para {requiredMinValue} ate {requiredMaxValue}.");
+            Debug.Log($"[TEMP] Valor da porta: {requiredMinValue} ate {requiredMaxValue}.");
+        }
+    }
 
     public void ReceiveValueFromPad(GameObject sourcePad, int receivedValue, GameObject sourceBlock)
     {
@@ -20,14 +51,14 @@ public class DoorValueVerifier : MonoBehaviour
         }
 
         string blockName = sourceBlock != null ? sourceBlock.name : "bloco desconhecido";
-        if (receivedValue == requiredValue)
+        if (receivedValue >= RequiredMinValue && receivedValue <= RequiredMaxValue)
         {
-            Debug.Log($"Verificador {name}: valor certo ({receivedValue}) recebido de {blockName}.");
+            Debug.Log($"Verificador {name}: valor certo ({receivedValue}) recebido de {blockName}. Intervalo aceito: {RequiredMinValue} ate {RequiredMaxValue}.");
             PlaySuccessParticles();
         }
         else
         {
-            Debug.Log($"Verificador {name}: valor errado ({receivedValue}). Valor necessario: {requiredValue}.");
+            Debug.Log($"Verificador {name}: valor errado ({receivedValue}). Intervalo necessario: {RequiredMinValue} ate {RequiredMaxValue}.");
         }
     }
 
